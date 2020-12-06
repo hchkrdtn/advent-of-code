3e3e9013e499691e60e4fd67d5690659dffafc99
MO <ondrejce@illinois.edu>
2020-12-06 16:54:24 -0600

    advent 5

--- 2020/d05.py
+++ 2020/d05.py
@@ -3,6 +3,40 @@
 import numpy as np
 
 
+def advent_5a(input_codes):
+    seat_max = 0
+    for code in input_codes:
+        row = code[0:7].replace("B", "1").replace("F", "0")
+        seat = code[-3:].replace("R", "1").replace("L", "0")
+        # binary to decimal
+        row = int(row, 2)
+        seat = int(seat, 2)
+        seat_id = row * 8 + seat
+        if seat_id > seat_max:
+            seat_max = seat_id
+    return seat_max
+
+
+def advent_5b(input_codes):
+    plane = np.zeros(len(input_codes))
+    for idx, code in enumerate(input_codes):
+        row = code[0:7].replace("B", "1").replace("F", "0")
+        seat = code[-3:].replace("R", "1").replace("L", "0")
+        seat_id = int(row, 2) * 8 + int(seat, 2)
+        plane[idx] = seat_id
+    plane.sort()
+    # print(plane)
+
+    # subtract adjacent numbers,
+    # all numbers should be 1 except around the missing number (and first and last maybe)
+    missing = np.subtract(plane, np.roll(plane, +1))
+    # print(missing)
+    my_seat = np.where(missing == 2)
+    # print(my_seat, plane[my_seat[0]])
+    return int(plane[my_seat[0]]-1)
+
+
+# brute force
 def get_id(code):
     # FBFBBFF RLR
     rows = list(code[:-3])
@@ -26,23 +60,23 @@ def get_id(code):
     seat = [0, 7]
     for idx, let in enumerate(rows):
         d = 2 ** (len(rows) - (idx + 1))
-        if let == "F":
-            row[1] -= d
-        elif let == "B":
+        # if let == "F":
+        #     row[1] -= d
+        if let == "B":
             row[0] += d
 
     for idx, dir in enumerate(seats):
         d = 2 ** (len(seats) - (idx + 1))
-        if dir == "L":
-            seat[1] -= d
-        elif dir == "R":
+        # if dir == "L":
+        #     seat[1] -= d
+        if dir == "R":
             seat[0] += d
     seat_id = row[0] * 8 + seat[0]
     # print([row[0], seat[0]], seat_id)
     return seat_id
 
 
-def advent_5a(input_codes):
+def advent_5a_brute(input_codes):
     seat_max = 0
     for code in input_codes:
         seat_id = get_id(code)
@@ -51,14 +85,12 @@ def advent_5a(input_codes):
     return seat_max
 
 
-def advent_5b(input_codes):
+def advent_5b_brute(input_codes):
     plane = np.zeros(len(input_codes))
     for idx, code in enumerate(input_codes):
         seat_id = get_id(code)
         plane[idx] = seat_id
     plane.sort()
-    # print(plane)
-
     # subtract adjacent numbers,
     # all numbers should be 1 except around the missing number (and first and last maybe)
     missing = np.subtract(plane, np.roll(plane, +1))
@@ -75,6 +107,7 @@ if __name__ == "__main__":
 
     test = False
     if test:
+        # test only for 5a
         input_codes = []
         input_codes.append("FBFBBFFRLR")
         input_codes.append("BFFFBBFRRR")
@@ -93,7 +126,9 @@ if __name__ == "__main__":
     print("max BBBBBBBRRR:",str(127 * 8 + 7))
 
     print(advent_5a(input_codes))
+    # print(advent_5a_brute(input_codes))
     print(advent_5b(input_codes))
+    # print(advent_5b_brute(input_codes))
 
     end_time = time.time()
     elapsed = end_time - start_time
