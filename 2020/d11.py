50f41da5b36cc76a63f596215eb024c46239404a
MO <ondrejce@illinois.edu>
2020-12-11 03:29:57 -0600

    advent 11

--- 2020/d11.py
+++ 2020/d11.py
@@ -1,11 +1,112 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import scipy
-from scipy import misc
 
 
-def run_kernel(mtx, round):
+def get_directions(mtx, i, j):
+    kern = np.array([[0,0,0],[0,0,0],[0,0,0]])
+    # print(mtx[i - 1:i + 2, j - 1:j + 2])
+    kern[1, 1] = mtx[i, j]
+    # print("down i+")
+    for idx in range(1, mtx.shape[0]-i-1):
+        cell = mtx[i+idx:i+idx+1, j:j+1]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[2, 1] = 2
+            break
+        if cell[0][0] == 1:
+            kern[2, 1] = 1
+            break
+        if len(cell) == 0:
+            break
+    # print("up i-")
+    for idx in range(0, i):
+        cell = mtx[i-idx-1:i-idx, j:j+1]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[0, 1] = 2
+            break
+        if cell[0][0] == 1:
+            kern[0, 1] = 1
+            break
+        if len(cell) == 0:
+            break
+    # print("right j+")
+    for idx in range(1, mtx.shape[1]-j-1):
+        cell = mtx[i:i+1, j+idx:j+idx+1]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[1, 2] = 2
+            break
+        if cell[0][0] == 1:
+            kern[1, 2] = 1
+            break
+        if len(cell) == 0:
+            break
+    # print("left j-")
+    for idx in range(0, j):
+        cell = mtx[i:i+1, j-idx-1:j-idx]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[1, 0] = 2
+            break
+        if cell[0][0] == 1:
+            kern[1, 0] = 1
+            break
+        if len(cell) == 0:
+            break
+    # print("diagonal down right i+j+")
+    for idx in range(1, np.min([mtx.shape[0]-i-1, mtx.shape[1]-j-1])):
+        cell = mtx[i+idx:i+idx+1, j+idx:j+idx+1]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[2, 2] = 2
+            break
+        if cell[0][0] == 1:
+            kern[2, 2] = 1
+            break
+        if len(cell) == 0:
+            break
+    # print("diagonal down left i+j-")
+    for idx in range(0, np.min([mtx.shape[0]-i-1, j])):
+        cell = mtx[i+idx+1:i+idx+2, j-idx-1:j-idx]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[2, 0] = 2
+            break
+        if cell[0][0] == 1:
+            kern[2, 0] = 1
+            break
+        if len(cell) == 0:
+            break
+    # print("diagonal top right i-j+")
+    for idx in range(0, np.min([i, mtx.shape[1]-j-1])):
+        cell = mtx[i-idx-1:i-idx, j+idx+1:j+idx+2]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[0, 2] = 2
+            break
+        if cell[0][0] == 1:
+            kern[0, 2] = 1
+            break
+        if len(cell) == 0:
+            break
+    # print("diagonal top left i-j-")
+    for idx in range(0, np.min([i, j])):
+        cell = mtx[i-idx-1:i-idx, j-idx-1:j-idx]
+        # print(cell[0][0])
+        if cell[0][0] == 2:
+            kern[0, 0] = 2
+            break
+        if cell[0][0] == 1:
+            kern[0, 0] = 1
+            break
+        if len(cell) == 0:
+            break
+    return kern
+
+
+def run_kernel_a(mtx, round):
     (dx, dy) = np.shape(mtx)
     mtx_n = np.array(mtx, copy=True)
     # print(round)
@@ -22,77 +123,61 @@ def run_kernel(mtx, round):
                 mtx_n[i, j] = 1
     if np.all(np.equal(mtx_n, mtx)):
         print(mtx_n)
-        print("DONE")
         return np.count_nonzero(mtx_n == 2)
     else:
-        return run_kernel(mtx_n, round+1)
+        return run_kernel_a(mtx_n, round+1)
+
+
+def run_kernel_b(mtx, round):
+    (dx, dy) = np.shape(mtx)
+    mtx_n = np.array(mtx, copy=True)
+    print(round)
+    # print(mtx_n)
+    for i in range(1, dx - 1):  # the range starts from 1 to avoid the column and row of zeros, and ends before the last col and row of zeros
+        for j in range(1, dy - 1):
+            if mtx[i:i+1, j:j+1] == 0:
+                continue
+            kern = get_directions(mtx, i, j)
+            # if i == 2 and j == 1:
+            #     print(kern)
+            occup = np.count_nonzero(kern == 2)
+            if kern[1, 1] != 2 and occup == 0:
+                mtx_n[i, j] = 2
+            elif kern[1, 1] == 2 and occup - 1 >= 5:
+                mtx_n[i, j] = 1
+    if np.all(np.equal(mtx_n, mtx)):
+        print(mtx_n)
+        return np.count_nonzero(mtx_n == 2)
+    else:
+        return run_kernel_b(mtx_n, round+1)
+
 
 def advent_11a(input_seats):
     # print(input_seats)
 
     tmp_char = np.array(input_seats)
     grid = tmp_char.astype(np.int)
-    print(grid)
+    # print(grid)
     (x, y) = np.shape(grid)
     grid_n = np.zeros([x+2, y+2]).astype(int)
     grid_n[1:x+1, 1:y+1] = grid
 
-    grid_res = run_kernel(grid_n, 0)
+    grid_res = run_kernel_a(grid_n, 0)
     return grid_res
 
 
-def get_recursively(diff, results, comb, intr):
-    for j in range(0, len(diff)-intr):
-        dd = np.asarray(diff[len(diff) - j - intr - 1:len(diff) - j])
-        patt = np.repeat(1, intr-1)
-        if np.all(np.equal(dd[1:intr], patt)) and dd[0:1][0] != 1 and dd[intr:intr+1][0] != 1:
-            if comb in results:
-                results[comb] += 1
-            else:
-                results[comb] = 1
-    if intr >= len(diff):
-        return results
-    else:
-        comb += intr - 1
-        get_recursively(diff, results, comb, intr+1)
-    return results
-
-    # for j in range(0, len(diff)-4):
-    #     dd = np.asarray(diff[len(diff) - j - 5:len(diff) - j])
-    #     print(dd)
-    #     if np.all(np.equal(dd[1:4], np.asarray([1, 1, 1]))) and dd[0:1][0] != 1 and dd[4:5][0] != 1:
-    #         if 4 in results:
-    #             results[4] += 1
-    #         else:
-    #             results[4] = 1
-    #
-    # for j in range(0, len(diff)-5):
-    #     dd = np.asarray(diff[len(diff) - j - 6:len(diff) - j])
-    #     print(dd)
-    #     if np.all(np.equal(dd[1:5], np.asarray([1, 1, 1, 1]))) and dd[0:1][0] != 1 and dd[5:6][0] != 1:
-    #         if 7 in results:
-    #             results[7] += 1
-    #         else:
-    #             results[7] = 1
-
-
 def advent_11b(input_seats):
-    # add zero
-    input_nums = np.asarray(input_seats)
-    nums = np.sort(input_nums)
-    nums = np.insert(nums, 0, 0)
-    nums = np.append(nums, nums[-1] + 3)
-    print(nums)
-    results = {}
-    num_roll = np.roll(nums, 1)
-    diff = nums - num_roll
-    print(diff)
-    tt = get_recursively(diff, results, 2, 3)
-    print(tt)
-    tot = 1
-    for key in tt:
-        tot *= key ** tt[key]
-    return tot
+    # print(input_seats)
+    tmp_char = np.array(input_seats)
+    grid = tmp_char.astype(np.int)
+    # print(grid)
+    (x, y) = np.shape(grid)
+    grid_n = np.zeros([x+2, y+2]).astype(int)
+    grid_n[1:x+1, 1:y+1] = grid
+
+    # print(get_directions(grid_n, 2, 1))
+    grid_res = run_kernel_b(grid_n, 0)
+    return grid_res
 
 
 if __name__ == "__main__":
@@ -134,7 +219,7 @@ if __name__ == "__main__":
 
 
     print(advent_11a(input_seats))
-    # print(advent_11b(input_seats))
+    print(advent_11b(input_seats))
 
     end_time = time.time()
     elapsed = end_time - start_time
