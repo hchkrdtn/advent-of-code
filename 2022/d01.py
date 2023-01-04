4b2ed5fbe4856e320477db9192edce5ff09850c8
hchkrdtn <za4rynsky@gmail.com>
2023-01-04 00:16:21 -0600

    Update d01.py

--- 2022/d01.py
+++ 2022/d01.py
@@ -3,12 +3,15 @@
 import numpy as np
 
 
-def advent_1a(arr):
-    return len(arr)
+def advent_a(arr):
+
+    return np.amax(np.array(arr))
 
 
-def advent_1b(arr):
-    return len(arr)
+def advent_b(arr):
+    arr = np.sort(arr, axis=None)
+
+    return np.sum(arr[-3:])
 
 
 if __name__ == "__main__":
@@ -26,8 +29,23 @@ if __name__ == "__main__":
             arr = [x.strip() for x in arr]
         f.close()
 
-    print(advent_1a(arr))
-    print(advent_1b(arr))
+    narr = []
+    nsum = 0
+    i = 0
+    while i < len(arr):
+        if arr[i] == "":
+            narr.append(nsum)
+            nsum = 0
+        else:
+            nsum += int(arr[i])
+
+        if i == len(arr) - 1:
+            narr.append(nsum)
+            break
+        i += 1
+
+    print(advent_a(narr))
+    print(advent_b(narr))
 
     end_time = time.time()
     elapsed = end_time - start_time
