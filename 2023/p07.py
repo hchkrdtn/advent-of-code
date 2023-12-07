7fb70cc29601011cfd6c7d8c052f9119a3a359d5
mondrejc <ondrejce@illinois.edu>
2023-12-07 09:57:01 -0600

    day 7 both solutions

--- 2023/p07.py
+++ 2023/p07.py
@@ -1,55 +1,120 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import re
 
 
 def advent_a(arr):
-    race_ts = re.findall(pattern=r"\d+", string=arr[0])
-    distances = re.findall(pattern=r"\d+", string=arr[1])
-    # print(race_ts)
-    # print(distances)
+    labels = {"A": 13, "K": 12, "Q": 11, "J": 10, "T": 9,
+              "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2, "2": 1}
+    # strength = {"Five": [5],
+    #             "Four": [4, 1],
+    #             "Full": [3, 2],
+    #             "Three": [3, 1, 1],
+    #             "Two": [2, 2, 1],
+    #             "One": [2,1,1,1],
+    #             "High": [1, 1, 1, 1, 1]}
+    strength = [[1, 1, 1, 1, 1], [2, 1, 1 ,1], [2, 2, 1], [3, 1, 1], [3, 2], [4, 1], [5]]
 
-    tot = 1
-    for i in range (0, len(race_ts)):
-        race_t = int(race_ts[i])
-        dist = int(distances[i])
+    res = []
+    for i, game in enumerate(arr):
+        game = game.split(" ")
 
-        race = np.zeros((race_t+1, 3), dtype=int)
-        race[:,0] = np.arange(race_t + 1)
-        race[:, 1] = np.flip(race[:, 0])
-        race[:, 2] = np.array((race[:, 0] * race[:, 1]))
-        # race[:, 3] = race[:, 2] - dist
+        # [index_strength, n1, n2, n3, n4, n5, int(game[1]), i]
+        # where 32T3K = 2 1 10 2 12 in numbers n1-n5, numbers are needed
+        # for soring
+        tmp = [0, 0, 0, 0, 0, 0, int(game[1]), i]
+        cnt = {}
+        for k in range(0, 5):
+            let = game[0][k:k+1]
+            if let in cnt:
+                cnt[let] += 1
+            else:
+                cnt[let] = 1
+            tmp[k+1] = labels[let]
 
-        record = (sum(race[:, 2] > dist))
-        tot *= record
+        hand_idx = list(cnt.values())
+        hand_idx.sort(reverse=True)
+
+        idx = [ind for ind, ele in enumerate(strength) if ele == hand_idx]
+        tmp[0] = idx[0]
+
+        res.append(tmp)
+    # print(res)
+    # complex sorting by 1 + 5 digits
+    res.sort(key=lambda res: (res[0], res[1], res[2], res[3], res[4], res[5]))
+    # print(res)
+
+    tot = 0
+    for i, r in enumerate(res):
+        tot += (i + 1) * r[-2]
+    # 765 * 1 + 220 * 2 + 28 * 3 + 684 * 4 + 483 * 5 = 6440
     return tot
 
 
 def advent_b(arr):
-    race_ts = re.findall(pattern=r"\d+", string=arr[0])
-    distances = re.findall(pattern=r"\d+", string=arr[1])
-    # print(race_ts)
-    # print(distances)
-    tot = 1
+    labels = {"A": 13, "K": 12, "Q": 11, "J": 0, "T": 9,
+              "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2, "2": 1}
+    strength = [[1, 1, 1, 1, 1], [2, 1, 1 ,1], [2, 2, 1], [3, 1, 1], [3, 2], [4, 1], [5]]
 
-    race_t = ""
-    dist = ""
-    for i in range (0, len(race_ts)):
-        race_t += race_ts[i]
-        dist += distances[i]
+    res = []
+    for i, game in enumerate(arr):
+        game = game.split(" ")
 
-    race_t = int(race_t)
-    dist = int(dist)
+        tmp = [0, 0, 0, 0, 0, 0, int(game[1]), i]
+        cnt = {}
+        for k in range(0, 5):
+            let = game[0][k:k+1]
+            if let in cnt:
+                cnt[let] += 1
+            else:
+                cnt[let] = 1
+            tmp[k+1] = labels[let]
 
-    race = np.zeros((race_t+1, 3), dtype=int)
-    race[:,0] = np.arange(race_t + 1)
-    race[:, 1] = np.flip(race[:, 0])
-    race[:, 2] = np.array((race[:, 0] * race[:, 1]))
-    # race[:, 3] = race[:, 2] - dist
+        hand_idx = list(cnt.values())
+        hand_idx.sort(reverse=True)
 
-    record = (sum(race[:, 2] > dist))
-    tot *= record
+        # Look up table for replacing combinations with J
+        # if "J" in cnt:
+        #     print(cnt["J"])
+        if "J" in cnt and cnt["J"] == 1:
+            if hand_idx == [1, 1, 1, 1, 1]:
+                hand_idx = [2, 1, 1, 1]
+            elif hand_idx == [2, 1, 1, 1]:
+                hand_idx = [3, 1, 1]
+            elif hand_idx == [2, 2, 1]:
+                hand_idx = [3, 2]
+            elif hand_idx == [3, 1, 1]:
+                hand_idx = [4, 1]
+            elif hand_idx == [4, 1]:
+                hand_idx = [5]
+        elif "J" in cnt and cnt["J"] == 2:
+            if hand_idx == [2, 1, 1, 1]:
+                hand_idx = [3, 1, 1]
+            elif hand_idx == [2, 2, 1]:
+                hand_idx = [4, 1]
+            elif hand_idx == [3, 2]:
+                hand_idx = [5]
+        elif "J" in cnt and cnt["J"] == 3:
+            if hand_idx == [3, 1, 1]:
+                hand_idx = [4, 1]
+            elif hand_idx == [3, 2]:
+                hand_idx = [5]
+        elif "J" in cnt and cnt["J"] == 4:
+            if hand_idx == [4, 1]:
+                hand_idx = [5]
+
+        idx = [ind for ind, ele in enumerate(strength) if ele == hand_idx]
+        tmp[0] = idx[0]
+
+        res.append(tmp)
+    # print(res)
+    res.sort(key=lambda res: (res[0], res[1], res[2], res[3], res[4], res[5]))
+    # print(res)
+
+    tot = 0
+    for i, r in enumerate(res):
+        tot += (i + 1) * r[-2]
+    # 765 * 1 + 220 * 2 + 28 * 3 + 684 * 4 + 483 * 5 = 6440
     return tot
 
 
@@ -58,9 +123,13 @@ if __name__ == "__main__":
 
     start_time = time.time()
 
-    test = False
+    test = True
     if test:
-        arr_a = ["seeds"]
+        arr_a = ["32T3K 765",
+                 "T55J5 684",
+                 "KK677 28",
+                 "KTJJT 220",
+                 "QQQJA 483"]
         arr = arr_a
         pass
     else:
